e7453dcea3eb4bd2ad719fc59af060fcdda25ce8
KingMelanie <79367937+KingMelanie@users.noreply.github.com>
2024-03-28 21:27:33 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-import os
-import json
-from dotenv import load_dotenv
 from openai import OpenAI
-# Load environment variables from .env file
-load_dotenv()
-
-# Now access the API key
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-# Configure the model ID. Change this to your model ID.
-model = "ft:gpt-3.5-turbo-0125:learninggpt::97U2upri"
-
-def extract_last_message(json_strings):
-    for json_string in json_strings:
-        data = json.loads(json_string)
-        last_message_content = data['messages'][-1]['content'].strip()
-        print(last_message_content)
+import os
 
 
-# Returns a drug class for each drug
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-json_strings = [
-        '{"messages": [{"role": "system", "content": "You are professional drug classifier."}, {"role": "user", "content": "Drug: A CN Gel(Topical) 20gmA CN Soap 75gm\\nMalady"}, {"role": "assistant", "content": " 0"}]}',
-        '{"messages": [{"role": "system", "content": "You are profesional drug classifier."}, {"role": "user", "content": "Drug: Provake 100mg Tablet 10SProvake 200mg Tablet 10S\\nMalady"}, {"role": "assistant", "content": " 1"}]}',
-        '{"messages": [{"role": "system", "content": "You are profesional drug classifier."}, {"role": "user", "content": "Drug: 1 AL Plus Capsule 10S1 AL 10mg Tablet 15S1 AL 5mg Tablet 10S1 AL Max Tablet 10S1 AL Syrup 30ml\\nMalady"}, {"role": "assistant", "content": " 2"}]}',
-    ]
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
 
-extract_last_message(json_strings)
+
+FINE_TUNED_MODEL="ft:babbage-002:learninggpt::97xSaVZt"
+YOUR_PROMPT="What is the remote for?"
+
+response = client.completions.create(
+    model=FINE_TUNED_MODEL,
+    prompt=YOUR_PROMPT,
+
+    temperature = 0.5,
+    max_tokens=25,
+
+)
+
+result = response.choices[0].text.strip()
+print(result)
+
